c66b5e55aedb4de06d7a480e705dc7c4b75147c4
odeshpande <odeshpande@RENTLAPT0109>
2019-08-06 16:31:58 +0530

    TriFibonacci solution added

--- CodeJam/TriFibonacci.cs
+++ CodeJam/TriFibonacci.cs
@@ -8,7 +8,9 @@ namespace Codejam
     {
         public int Complete(int[] test)
         {
-            return -1;
+            int index = Array.IndexOf(test, -1);
+            TriFiboSeries series = new TriFiboSeries(test);
+            return (series.CheckTriFiboSeries(test))?series.GetNumberAtIndex(index):-1;
         }
 
         #region Testing code Do not change
@@ -26,5 +28,52 @@ namespace Codejam
             } while (input != "-1");
         }
         #endregion
+    }
+
+    public class TriFiboSeries
+    {
+        int[] baseSeries= new int[3];
+
+        public TriFiboSeries(int[] fiboseries)
+        {
+            
+            for(int i =0;i<3;i++)
+                if(fiboseries[i]==-1)
+                    fiboseries[i]= fiboseries[3]- fiboseries[0] - fiboseries[1] - fiboseries[2] -1;
+            Array.Copy(fiboseries, baseSeries, 3);
+        }
+
+        public bool CheckTriFiboSeries(int[] fiboseries)
+        {
+            for(int i = 0; i < fiboseries.Length; i++)
+            {
+                if (fiboseries[i] == GetNumberAtIndex(i) || fiboseries[i]==-1)
+                    continue;
+                return false;
+            }
+            return true;
+        }
+
+        public int GetNumberAtIndex(int index)
+        {
+            if (index < 3)
+                return (baseSeries[index]>0)?baseSeries[index]:-1;
+            int i = 3;
+            int temp=0,temp1= baseSeries[0], temp2 = baseSeries[1], temp3= baseSeries[2];
+            if (index == 0 || index == 1 || index == 2)
+            {
+                return baseSeries[index];
+            }
+
+            while (i <= index)
+            {
+                temp = temp3;
+                temp3 = temp + temp1 + temp2;
+                temp1 = temp2;
+                temp2 = temp;
+                i++;
+            }
+            return (temp3>0)?temp3:-1;
+        }
     }
 }
